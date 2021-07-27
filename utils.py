19ee9f9d9207a3bacf472f9ec146c729b19ad931
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-07-27 10:03:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -42,17 +42,17 @@ class Msg:
     def start(msg: Message) -> str:
         """ return start-message text """
         txt = f"Hey {msg.from_user.mention}!\n" \
-              "\nI can compress files in to an archive." \
+              "\nI can compress files in to an archive.🤐" \
               "\nJust send /zip, and follow the instructions."
         return txt
 
     zip = "Send the files you want to compress, and at the end send /stopzip after all the files have been downloaded.\n" \
-          "\n\nNote: due to upload limit, the total size of the file(s) can be at most 2GB."
-    too_big = "Note: due to upload limit, the total size of the file(s) can be at most 2GB."
+          "\n\nNote: due to upload limit, the total size of the file(s) can be at most 1GB."
+    too_big = "Note: due to upload limit, the total size of the file(s) can be at most 1GB."
     too_much = "Note: the total number of the files can be at most 500"
-    send_zip = "Send /zip to compress the files"
-    zipping = "start compressing {} files..."
-    uploading = "uploading archive..."
-    unknow_error = "An unknown error occurred"
-    downloading = "downloading..."
-    zero_files = "No files were sent"
+    send_zip = "Send /zip to compress the files🗜️"
+    zipping = "🕣start compressing {} files...🕝"
+    uploading = "📤uploading archive📤..."
+    unknow_error = "🚫An unknown error occurred🚫"
+    downloading = "📥downloading...📥"
+    zero_files = "🔍No files were sent"
